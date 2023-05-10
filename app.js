38ddcb6ebcbe922caab6da83c722a5ccec63f9a1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-10 19:56:01 +0100

    Created data()

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const app = Vue.createApp({
     //data, functions
-template: '<h2> I am a template </h2>'
+// template: '<h2> I am a template </h2>'
+data() {
+    return {
+        title: 'The final empire'
+    }
+}
 })
 app.mount('#app')
